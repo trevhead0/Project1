928eac96b4b14bb8a4f71e45190b762e454f9b12
Trevhead0 <tloveday@hawk.iit.edu>
2019-02-17 04:02:30 -0600

    Update index and JS files   index-     Add 3 nav point id's   JS file-     Add Variables for nav buttons     Add variables for next buttons     Add variable for submit button     good to go navigation

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,43 +1,67 @@
-(function(){
-  // Browser sanity check:
+(function(){  // Browser sanity check:
   if (!('querySelector' in document && 'addEventListener' in document)) {
     // Old, old browser.
-    console.log('Old browser, or you turned off js. Noice');//not afraid to put it
+    console.log('Old browser, or you turned off js. Noice'); // not afraid to put it
     return;
   }
+  else
+  {
+    console.log("we are good");
+  }
+})();
+// var menu = document.querySelector('#signup-process');
+document.addEventListener('DOMContentLoaded', function(){
+  var my_submit=document.querySelector('#submitbut');
+  var my_name=document.querySelector('#actual-name');
+  var my_business=document.querySelector('#business');
+  var my_services=document.querySelector('#services');
+  var page=0;
+  console.log('OMG the DOM is loaded!!!!');// I do not know why you hide these
+  // Disable the submit button until we are reasonable sure
+  // that we have a ten-digit phone number
+  my_submit.setAttribute('disabled', 'disabled');
+  // menu.classList.toggle('nv');
+  document.querySelector('#namenext').classList.toggle('nv');
+  document.querySelector('#businessnext').classList.toggle('nv');
+  my_business.classList.toggle('nv');
+  my_services.classList.toggle('nv');
 
-  var my_submit = document.querySelector('#submit');
-  var menu = document.querySelector('#signup-process');
-
-  document.addEventListener('DOMContentLoaded',function(){
-    console.log('OMG the DOM is loaded!!!!');//I do not know why you hide these
-    // Disable the submit button until we are reasonable sure
-    // that we have a ten-digit phone number
-    menu.classList.toggle('nv');
-    my_submit.setAttribute('disabled','disabled');
-  });
-
-  var tel_input = document.querySelector('#telephone');
-  tel_input.addEventListener('focus', function(){
-    console.log('OMG somebody focused on the telephone input');
-  });
-  tel_input.addEventListener('blur', function(){
-    console.log('OMG somebody navigated away from the telephone input');
-  });
-
-  tel_input.addEventListener('keyup',function(){
-    // Remove all non-digit characters from the telephone input's value
-    var clean_number = this.value.replace(/\D/g,'');
-    // Remove any 1 apppearing at the start of the number
-    var sanitized_number = clean_number.replace(/^1/,'');
-    // Finally, check to see if the number is 10 digits long
-    if (sanitized_number.length === 10) {
-      console.log('That number looks great!');
-      // If so, allow the form to be submitted
-      phone_submit.removeAttribute('disabled');
+  document.addEventListener('click', function(e){
+    if (e.target===document.querySelector('#next1') || (e.target===document.querySelector('#navbusiness')&&page!==1)){
+      if(page===0)
+      {
+        my_name.classList.toggle('nv');
+      }
+      else
+      {
+        my_services.classList.toggle('nv');
+      }
+      my_business.classList.toggle('nv');
+      page=1;
     }
-
+    else if(e.target===document.querySelector('#navname')&&page!==0){
+      if (page===1)
+      {
+        my_business.classList.toggle('nv');
+      }
+      else
+      {
+        my_services.classList.toggle('nv');
+      }
+      my_name.classList.toggle('nv');
+      page=0;
+    }
+    else if(e.target===document.querySelector('#next2') || (e.target===document.querySelector('#navservices')&&page!==2)){
+      if (page===1)
+      {
+        my_business.classList.toggle('nv');
+      }
+      else
+      {
+        my_name.classList.toggle('nv');
+      }
+      my_services.classList.toggle('nv');
+      page=2;
+    }
   });
-
-
-}());
+});
