8f2e2f277fe02df3542259d4b6a4802e8e60b83c
Trevhead0 <tloveday@hawk.iit.edu>
2019-03-02 23:40:10 -0600

    Remove 2 boxes in exchange for 1   -Delete phone and all parameters Index   -Make email into contact   -Correct error message to include email and phone JS   -Sexify Javascript...how you doin   -throw all/or most functions out of domload function; CSS   -Add contact parameters.   -Change Max width to include multi functionality

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,149 +6,125 @@
   }
 
   document.addEventListener('DOMContentLoaded', function(){
-
     // Make this top of everything!
     // Declare variables
-    var gtgn=false;
-    var gtge=false;
-    var gtgp=false;
+    var form=document.querySelector('#form');
     var submit=document.querySelector('#submit');
     var yourname=document.querySelector('#yourname');
-    var email=document.querySelector('#youremail');
-    var phone=document.querySelector('#phonenumber');
+    var contact=document.querySelector('#yourcontact');
 
     // hide submit until ready!
     // declare event listeners for focus and blur
     yourname.addEventListener('focus', focus);
-    email.addEventListener('focus', focus);
-    phone.addEventListener('focus', focus);
+    contact.addEventListener('focus', focus);
     // blur is meant for throwing an error after the user is finished typing.
     yourname.addEventListener('blur', blur);
-    email.addEventListener('blur', blur);
-    phone.addEventListener('blur', blur);
+    contact.addEventListener('blur', blur);
     submit.setAttribute('disabled', 'disabled');
 
-    // Name validation
-    yourname.addEventListener('input', function(){
-      if (this.value===''){
-        gtgn=false;}
-      else {
-        gtgn=true;
-      }
-      check();
+    form.addEventListener('keyup', function(){
+      var name_value=yourname.value;
+      var contact_value=contact.value;
+      check(namecheck(name_value), emailcheck(contact_value), phonecheck(contact_value));
     });
-
-    // this is email validation with regex
-    email.addEventListener('input', function(){
-      // regex makes alpha1 or more>>with an @ sign>> with another 1 or more alpha//a dot//and another 1 or more alpha
-      var re= (/\w+@\w+\.\w+/g);
-      if (this.value.match(re)){
-        console.log(this.value);
-        gtge=true;}
-      else {
-        gtge=false;
-      }
-      check();
-    });
-
-    // this is validation for phone number
-    phone.addEventListener('input', function(){
-      // Remove all non-digit characters from the telephone input's value
-      var clean_number = this.value.replace(/\D/g, '');
-      // Remove any 1 apppearing at the start of the number
-      var sanitized_number = clean_number.replace(/^1/, '');
-      // Finally, check to see if the number is 10 digits long
-      if (sanitized_number.length === 10) {
-        console.log('That number looks great!');
-        // If so, allow the form to be submitted
-        gtgp=true;
-      }
-      else {
-        gtgp=false;
-      }
-      check();
-    });
-
-    // check function for enabling submit
-    function check(){
-      if (gtgn&&(gtgp===true || gtge===true)){
-        submit.removeAttribute('disabled');}
-      else{
-        submit.setAttribute('disabled', 'disabled');}
-    }
-
-    // toggle function for focus css
-    function focus(){
-      // console.log('yeah they click me!');
-      if (document.querySelector('.focus')!==null){// this is to throw out error when no focus
-        document.querySelector('.focus').classList.toggle('focus');}
-      this.classList.toggle('focus');
-    }
-
-    // final check and enable error messages.
-    function blur(){
-      var en = document.querySelector('.errorn');
-      var ee= document.querySelector('.errore');
-      var ep= document.querySelector('.errorp');
-      // check blur out to show error message on name
-      if(event.target===yourname){
-        if(gtgn===true){
-          if (en.classList.contains('nv')){
-            return;
-          }
-          else{
-            en.classList.toggle('nv');
-          }
-        }
-        else{
-          if(en.classList.contains('nv')){
-            en.classList.toggle('nv');
-          }
-          else{
-            return;
-          }
-        }
-      }
-      // check blur out to show error message on email
-      if(event.target===email){
-        if(gtge===true || email.value===""){
-          if (ee.classList.contains('nv')){
-            return;
-          }
-          else {
-            ee.classList.toggle('nv');
-          }
-        }
-        else{
-          if(ee.classList.contains('nv')){
-            ee.classList.toggle('nv');
-          }
-          else{
-            return;
-          }
-        }
-      }
-      // check blur out to show error message on phone
-      if(event.target===phone){
-        if(gtgp===true || phone.value===""){
-          console.log('gtgp is true ');
-          if (ep.classList.contains('nv')){
-            return;
-          }
-          else {
-            ep.classList.toggle('nv');
-          }
-        }
-        else{
-          if(ep.classList.contains('nv')){
-            ep.classList.toggle('nv');
-          }
-          else{
-            return;
-          }
-        }
-      }
-    }
   });
+  // toggle function for focus css
+  function focus(){
+    // console.log('yeah they click me!');
+    if (document.querySelector('.focus')!==null){// this is to throw out error when no focus
+      document.querySelector('.focus').classList.toggle('focus');}
+    this.classList.toggle('focus');
+  }
 
+  function namecheck(namecheck){
+    if (namecheck===''){
+      return false;}
+    else {
+      return true;
+    }
+  }
+
+  function phonecheck(contact){
+    var contact_value = contact;
+    // Remove all non-digit characters from the telephone input's value
+    var clean_number = contact_value.replace(/\D/g, '');
+    // Remove any 1 apppearing at the start of the number
+    var sanitized_number = clean_number.replace(/^1/, '');
+    // Finally, check to see if the number is 10 digits long
+    if (sanitized_number.length === 10) {
+      console.log('That number looks great!');
+      // If so, allow the form to be submitted
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  function emailcheck(contact){
+    // regex makes alpha1 or more>>with an @ sign>> with another 1 or more alpha//a dot//and another 1 or more alpha
+    var re= (/\w+@\w+\.\w+/g);
+    var contact_value= contact;
+    if (contact_value.match(re)){
+      console.log("email looks great!");
+      return true;}
+    else {
+      return false;
+    }
+  }
+
+  // check function for enabling submit
+  function check(gtgn, gtge, gtgp){
+    var submit=document.querySelector('#submit');
+    if (gtgn&&(gtge || gtgp)){
+      submit.removeAttribute('disabled');}
+    else{
+      submit.setAttribute('disabled', 'disabled');}
+  }
+
+  // final check and enable error messages.
+  function blur(){
+    var yourname=document.querySelector('#yourname');
+    var contact=document.querySelector('#yourcontact');
+    var en = document.querySelector('.errorn');
+    var ec= document.querySelector('.errorc');
+
+    // check blur out to show error message on name
+    if(event.target===yourname){
+      if(namecheck(yourname.value)){
+        if (en.classList.contains('nv')){
+          return;
+        }
+        else{
+          en.classList.toggle('nv');
+        }
+      }
+      else{
+        if(en.classList.contains('nv')){
+          en.classList.toggle('nv');
+        }
+        else{
+          return;
+        }
+      }
+    }
+    // check blur out to show error message on contact
+    if(event.target===contact){
+      if(emailcheck(contact.value)|| phonecheck(contact.value) || contact.value===""){
+        if (ec.classList.contains('nv')){
+          return;
+        }
+        else {
+          ec.classList.toggle('nv');
+        }
+      }
+      else{
+        if(ec.classList.contains('nv')){
+          ec.classList.toggle('nv');
+        }
+        else{
+          return;
+        }
+      }
+    }
+  }
 })();
